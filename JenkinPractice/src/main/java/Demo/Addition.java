293b0a8094b133f03543c7711b451d530124e755
JamunaManoj <jamunashankar11@gmail.com>
2023-07-12 11:44:07 +0530

    1st commit

---
+++ JenkinPractice/src/main/java/Demo/Addition.java
@@ -0,0 +1,17 @@
+package Demo;
+
+public class Addition {
+
+	public static void main(String[] args) 
+	{
+		
+		int a=1;
+		int b=2;
+		int c=a+b;
+		System.out.println(c);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
